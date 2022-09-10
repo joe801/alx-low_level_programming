ae57b49ee9916651d46ee1d138ee0c2ff5d489cf
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-10 21:09:59 +0000

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[16] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+		putchar(alp[i]);
+	putchar('\n');
+	return (0);
+}
