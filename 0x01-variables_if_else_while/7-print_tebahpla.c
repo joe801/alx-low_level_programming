000b7cf5438ed62d843002c2402c76237276f87f
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-10 21:04:10 +0000

    print reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar(alp[i]);
+	putchar('\n');
+	return (0);
+}
