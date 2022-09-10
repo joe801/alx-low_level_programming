dd44ff6627a64bce3c86f4c7f3685e65b9696c24
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-10 20:47:14 +0000

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+		putchar(alp[i]);
+	putchar('\n');
+	return (0);
+}
