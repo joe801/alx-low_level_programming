42949dac8833800fbde561b0bf07168b10917535
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-15 15:35:28 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @i: type int
+ *
+ * Description: returns last digit
+ * Return: last digit (success)
+ */
+int print_last_digit(int i)
+{
+	int last;
+
+	last = i % 10;
+	if (last < 0)
+	{
+		_putchar(-last + 48);
+		return (-last);
+	}
+	else
+	{
+		_putchar(last +48);
+		return (last);
+	}
+}
