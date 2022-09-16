5e116b72897557d8456b2e7df3b5de0fee47e741
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-16 23:28:40 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numers - Entry point
+ *
+ * prints numbers 1 to 14 10 times
+ *
+ * Return: Void (success)
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	j = 0;
+	while (j < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{
+			if (i >= 10)
+				_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			i++;
+		}
+		j++;
+		_putchar('\n');
+	}
+}
