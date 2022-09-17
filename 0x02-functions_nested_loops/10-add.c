1c6ff8e316e661b72676940cb0e4c6bff179b0e9
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-17 21:41:46 +0000

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Prints the 9 times table
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Description - for adding given integers in argument
+ *
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
