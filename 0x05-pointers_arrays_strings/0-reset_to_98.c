76da002634f33d609c8ece637945a7e49ff1cceb
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-21 12:37:21 +0000

    change pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ *
+ * changes value of pointer points to 98
+ *
+ * @n: pointer int
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
