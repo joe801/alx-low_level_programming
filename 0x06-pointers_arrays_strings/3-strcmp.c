49080261de9ad4c03e92cfc9327930ffafdbe454
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-22 21:40:14 +0000

    updated strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,24 +13,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j;
+	int i = 0, j = 0;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (j == 0)
 	{
-		if (s1[i] > s2[i])
-		{
-			j = 15;
+		if (s1[i] == '\0' || s2[i] == '\0')
 			break;
-		}
-		else if (s1[i] < s2[i])
-		{
-			j = -15;
-			break;
-		}
-		else
-		{
-			j = 0;
-		}
+		j = s1[i] - s2[i];
 		i++;
 	}
 	return (j);
