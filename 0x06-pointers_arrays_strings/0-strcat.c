b896028c3385b20a21817aa021db16301495dc7c
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-22 18:24:31 +0000

    concatenating 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - entry point
+ *
+ * concatenates two strings
+ *
+ * @dest: pointer char
+ * @src: pointer src
+ *
+ * Return: *dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, count2 = 0;
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		dest[count] = src[count2];
+		if (src[count2] == '\0')
+		{
+			break;
+		}
+		count++;
+		count2++;
+	}
+	return (dest);
+}
