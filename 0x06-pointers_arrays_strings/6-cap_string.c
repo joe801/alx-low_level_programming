7f4c8eb5d0a111eaab62c162c59b51bbb5433c3e
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-23 22:33:24 +0000

    updated capitalising2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,22 +11,24 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	int sep[] =  {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (s[i] != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		for (j = 0; j < 13; j++)
+		for (i = 0; i < 13; i++)
 		{
-			if (s[i] == sep[j])
+			if (*(s + count) == sep_words[i])
 			{
-				if (s[i + 1] >= 97 && s[i + 1] <= 122)
-					s[i + 1] = s[i + 1] - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-		if (s[i] == 9)
-			s[i] = 32;
-		i++;
+		count++;
 	}
 	return (s);
 }
