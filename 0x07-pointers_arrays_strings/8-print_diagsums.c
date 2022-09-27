bb852d36352b1562cf9ad8c9289b72bdb78bf9e1
Joseph Uwagbale <joecrypt@yahoo.com>
2022-09-27 22:36:09 +0000

    printing diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints diagonal sum of 2d array
+ *
+ * @a: pointer array
+ * @size: size of array
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, diag_a = 0, diag_b = 0;
+
+	i = 0;
+	while (i < (size * size))
+	{
+		if (i % (size + 1) == 0)
+			diag_a += a[i];
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			diag_b += a[i];
+		i++;
+	}
+	printf("%d, %d \n", diag_a, diag_b);
+}
