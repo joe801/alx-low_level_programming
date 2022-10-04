a548305cac95b9a27daeeb97c850aa5b3ea96a5b
Joseph Uwagbale <joecrypt@yahoo.com>
2022-10-04 20:37:31 +0000

    str concatenation2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int i, j, a = 0, b = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	i = 0;
 	while (s1[i] != '\0')
