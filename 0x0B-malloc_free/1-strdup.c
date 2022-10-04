9c347b47f97f35db5fb944916e78b3cb462981a0
Joseph Uwagbale <joecrypt@yahoo.com>
2022-10-04 19:51:37 +0000

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - copy string with memory from malloc
+ *
+ * @str: string to copy
+ *
+ * Return: copy of str
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i, len;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	s = malloc(sizeof(char) * len);
+	i = 0;
+	while (i < len)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	return (s);
+}
