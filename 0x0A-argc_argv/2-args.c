017971358291e48082fea9a7191eb0a76a4568e5
Joseph Uwagbale <joecrypt@yahoo.com>
2022-10-05 13:49:54 +0000

    printing argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints all program arguments
+ *
+ * @argc: number of arguments passed with program
+ * @argv: array of arguments passed with program
+ *
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
