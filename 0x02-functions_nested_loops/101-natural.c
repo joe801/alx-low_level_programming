0603d79e60b3ced3e4d54ce0c5c219f4d93808bc
Joseph Uwagbale <joecrypt@yahoo.com>
2022-10-08 22:17:51 +0000

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * prints sum of all numbers from 1 to 1024 that are multiples of 3 and 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	i = 0;
+	while (i < 1024)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+}
