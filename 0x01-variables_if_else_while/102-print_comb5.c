ff99ab8589f9703e2a05c68a416243305521693a
Joseph Uwagbale <joecrypt@yahoo.com>
2022-10-09 22:39:33 +0000

    updated four number combo for betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,17 +18,15 @@ int main(void)
 				{
 					if (((i * 10) + j) < ((k * 10) + l))
 					{
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(k);
-						putchar(l);
-						if (i == 57 && j == 56 && k == 57 && l == 57)
-						{
-							break;
-						}
-						putchar(',');
-						putchar(' ');
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 					}
 				}
 			}
