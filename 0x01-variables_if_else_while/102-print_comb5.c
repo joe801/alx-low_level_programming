c149e05c99af8a73f13e86e300946693a82934a2
Joseph Uwagbale <joecrypt@yahoo.com>
2022-10-09 22:32:51 +0000

    four number combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					if (((i * 10) + j) < ((k * 10) + l))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+						if (i == 57 && j == 56 && k == 57 && l == 57)
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
