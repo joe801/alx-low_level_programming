90d788a3dfa2e78c2b4c71f6726645bfd3884e4f
Joseph Uwagbale <joecrypt@yahoo.com>
2022-10-11 14:21:11 +0000

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - details of a dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
