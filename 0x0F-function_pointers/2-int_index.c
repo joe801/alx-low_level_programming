695ef94c1b29a7748c3fd15f5b3cc60c52b46f63
Joseph Uwagbale <joecrypt@yahoo.com>
2022-10-18 14:06:52 +0000

    searching for integer2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,13 +13,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
-		return (-1);
-	while (i < size)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
-		i++;
+		if (size <= 0)
+			return (-1);
+		while (i < size)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+			i++;
+		}
 	}
 	return (-1);
 }
