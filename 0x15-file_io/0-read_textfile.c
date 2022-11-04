f2d260da860b0058cce272e3aedb96db02e4f939
Joseph Uwagbale <joecrypt@yahoo.com>
2022-11-04 21:29:56 +0000

    reading file to output4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t i = 0;
+	ssize_t _read, _write;
 	char *buf = malloc(sizeof(char) * letters);
 
 	if (!buf)
@@ -20,14 +20,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	read(fd, buf, letters);
-	buf[letters] = '\0';
+	_read = read(fd, buf, letters);
+	_write = write(STDOUT_FILENO, buf, _read);
 	close(fd);
-	while (buf[i] != '\0')
-	{
-		_putchar(buf[i]);
-		i++;
-	}
 	free(buf);
-	return (i);
+	return (_write);
 }
