6beb44b787a6d59af97061758d2077501caf1bc6
Joseph Uwagbale <joecrypt@yahoo.com>
2022-11-04 23:07:22 +0000

    creating file2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		text_content = "";
 	write(fd, text_content, i);
 	close(fd);
 	return (1);
