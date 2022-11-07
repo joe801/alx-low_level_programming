db83f2ad28ac0ba74962a81466bf7bed5ef40115
Joseph Uwagbale <joecrypt@yahoo.com>
2022-11-07 20:26:33 +0000

    printing arrays 2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,13 @@
 void print_array(int *a, int n)
 {
 	int i = 0;
-
-	while (i < (n - 1))
+	
+	while (i < n)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+		if (i != (n -1))
+			printf(", ");
 		i++;
 	}
-	printf("%d\n", a[n - 1]);
+	printf("\n");
 }
